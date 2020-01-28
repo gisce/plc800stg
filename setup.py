2e1566b9bcd78268eb62ed2f8141d2ff3b03aaf8
ivan <iiglesias@gisce.net>
2020-01-28 11:24:50 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='plc800stg',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/gisce/plc800stg',
+    license='MIT License',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    install_requires=[
+        'lxml',
+    ],
+    description='PLC800 STG'
+)
